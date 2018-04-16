bda7acedc37bc764c03956d6c93cab4e59f43591
h-fujikura <h-fujikura@hands-lab.com>
2018-04-16 13:21:10 +0900

    calcTableをコンポーネントに修正

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,35 @@
 <template>
   <div id="demo">
-    <section>
-      <table>
-        <tr>
-          <th>商品名</th>
-          <th>単価</th>
-          <th>個数</th>
-          <th>行計</th>
-          <th></th>
-        </tr>
-        <tr v-for="(item, index) in items" 
-                :key="item.name">
-          <td>
-            <input type="text" v-model="item.name" >
-          </td>
-          <td>
-            <input type="number" v-model.number="item.tanka" >
-          </td>
-          <td>
-            <input type="number" v-model.number="item.qty" >
-          </td>
-          <td class="taRight" >
-            {{addComma(item.qty * item.tanka)}} 円
-          </td>
-          <td>
-            <button @click="removeData(index)">行削除</button>
-          </td>  
-        </tr>        
-        <tr>
-          <td colspan="2">計</td>
-          <td class="taRight">{{qtySum}}個</td>
-          <td class="taRight">{{addComma(uriageSum)}} 円</td>
-          <td></td>
-        </tr>
-        </table>
-      <button @click="addRow">
-        行追加
-      </button>
-    </section>
+    <calc-table :items="fruits"></calc-table>
   </div>
 </template>
 
 <script>
-const formatter = new Intl.NumberFormat('ja-JP');
+import calcTable from './calcTable'
 export default {
   name: "App",
   data () {
     return {
-      items: [
-      	{name: "りんご", tanka:100, qty:1 },
+      fruits: [
+       	{name: "りんご", tanka:100, qty:1 },
       	{name: "バナナ", tanka:250, qty:2 },
       	{name: "みかん", tanka:200, qty:3 },
-      	{name: "ぶどう", tanka:400, qty:4 }
-      ]
+      	{name: "ぶどう", tanka:400, qty:4 } 
+      ]      
     }
+  },
+  components: {
+    calcTable
   },
   methods: {
-		removeData(idx) {
-			this.items.splice(idx,1)
-		},
-		addRow(){
-      this.items.push(
-        {name: '', tanka: null, qty: null }
-      )
-		},
-    addComma(num) {
-      return formatter.format(num)
-    }
   },
   computed: {
-    qtySum() {
-      return this.items.reduce((pre, cur) => {
-        return  pre + cur.qty
-      },0)
-    },
-    uriageSum() {
-      return this.items.reduce((pre, cur) => {
-        return  pre + cur.qty * cur.tanka
-      },0)
-    }  
   }
 };
 </script>
 
 <style>
-#app {
+#demo {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -93,27 +38,5 @@ export default {
   margin-top: 60px;
   font-size: 16px;
 }
-section {
-  border-bottom: 2px solid #42b983;
-  padding: 1rem;
-}
-input {
-  text-align: right;
-  width: 90%;
-  font-size: 1rem;
-}
-table,
-td,
-th {
-  border: 1px solid #000;
-  border-collapse: collapse;
-}
-table {
-  table-layout: fixed;
-  width: 100%;
-  text-align: center;
-}
-td.taRight {
-  text-align: right;
-}
+
 </style>
